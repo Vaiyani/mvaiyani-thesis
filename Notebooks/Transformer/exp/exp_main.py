2b18cb9f7c4f693ff61ae70ba9ba24fe465562a2
Vaiyani <muneeb.vaiyani@gmail.com>
2022-08-03 22:04:49 +0200

    set cuda to default 0 (local machine specs)

--- Notebooks/Transformer/exp/exp_main.py
+++ Notebooks/Transformer/exp/exp_main.py
@@ -24,14 +24,12 @@ class Exp_Main(Exp_Basic):
 
     def _build_model(self):
         model_dict = {
-            'Autoformer': Autoformer,
-            'Transformer': Transformer,
-            'Informer': Informer
+            'Transformer': Transformer
         }
         model = model_dict[self.args.model].Model(self.args).float()
 
-        if self.args.use_multi_gpu and self.args.use_gpu:
-            model = nn.DataParallel(model, device_ids=self.args.device_ids)
+        # if self.args.use_multi_gpu and self.args.use_gpu:
+        #     model = nn.DataParallel(model, device_ids=self.args.device_ids)
         return model
 
     def _get_data(self, flag):

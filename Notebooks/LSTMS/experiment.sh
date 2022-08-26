7ed457c8eb21959c7f2adfc4a509e058159d4884
Vaiyani <muneeb.vaiyani@gmail.com>
2022-08-26 09:55:24 +0200

    script for LSTMS

---
+++ Notebooks/LSTMS/experiment.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#for lookback in 6 12 24 48 72 96 120 144 168 192 336 504 672 720
+for lookback in 720 672 504 336 192 168 144 120 96 72 48 24 12 6
+do
+  for future in 1 12 24 96 192 336 720
+  do
+    python main.py --lookback $lookback --future $future
+  done
+done
--- Notebooks/LSTMS/main.py
+++ Notebooks/LSTMS/main.py
@@ -1,30 +1,34 @@
 import pandas as pd
 import numpy as np
 import argparse
+import random
 import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '-1'
+import tensorflow as tf
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_absolute_percentage_error, mean_squared_error, mean_absolute_error, r2_score
 from tensorflow.keras.models import save_model, model_from_json, Sequential
 from tensorflow.keras.layers import Dense, LSTM, Dropout
 from tensorflow import keras
-from keras_tuner.engine.hyperparameters import HyperParameters
 from typing import Union
-import warnings
-warnings.simplefilter("ignore", UserWarning)
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 
+fix_seed = 2022
+tf.random.set_seed(fix_seed)
+np.random.seed(fix_seed)
 
+# sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True))
 parser = argparse.ArgumentParser(description='Time Series Forecasting With LSTMS')
-parser.add_argument('--lookback', type=int, required=False, default=10, help='past time step to look into')
+parser.add_argument('--lookback', type=int, required=False, default=6, help='past time step to look into')
 parser.add_argument('--future', type=int, required=False, default=1, help='time steps to predict in future')
 args = parser.parse_args()
 lookback = args.lookback
 pred_len = args.future
 identifier = 'LSTM_Hourly_lookback-{}_future-{}'.format(lookback, pred_len)
 print(identifier)
-isExist = os.path.exists('./saved_models')
-if not isExist:
-  os.makedirs('./saved_models')
+# isExist = os.path.exists('./saved_models')
+# if not isExist:
+#   os.makedirs('./saved_models')
 
 def min_max_scale(train: np.array, val: np.array, test: np.array) -> Union[MinMaxScaler, np.array, np.array, np.array]:
     """ Tranform the train and test data into min max scale of train data"""
@@ -69,12 +73,12 @@ def sliding_window(data: [], window_length: int, pred_len: int = 1) -> Union[np.
 def model():
 
     model = keras.Sequential()
-    model.add(LSTM(100, activation='relu', return_sequences=True))
+    model.add(LSTM(100, activation='tanh', return_sequences=True))
     model.add(Dropout(0.2))
-    model.add(LSTM(100, activation='relu'))
+    model.add(LSTM(100, activation='tanh'))
     model.add(Dense(pred_len))
 
-    model.compile(optimizer='adam',
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.0001),
                   loss='mse')
 
     return model
@@ -124,11 +128,12 @@ model = model()
 
 # stop_training_early = keras.callbacks.EarlyStopping()
 stop_training_early = keras.callbacks.EarlyStopping(monitor="val_loss", patience=3)
+# with tf.device('/cpu:0'):
 history = model.fit(x_train, y_train, epochs=60 , verbose=1, shuffle=False, validation_data=(x_val, y_val),
                     callbacks=[stop_training_early])
 
 
-## save model
-model.save('saved_model/' + identifier)
+## predict model and save result
+# model.save('saved_models/' + identifier)
 y_predict = model.predict(x_test)
 calculate_metrics(y_test, y_predict, identifier)

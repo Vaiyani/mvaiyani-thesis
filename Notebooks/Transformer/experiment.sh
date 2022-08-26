df5d789557ec8aaea9640d9be82cba32feaea2f4
Vaiyani <muneeb.vaiyani@gmail.com>
2022-08-26 09:56:58 +0200

    results and bash script for Transformer for all possible combination of embeddings, lookback and future

--- Notebooks/Transformer/experiment.sh
+++ Notebooks/Transformer/experiment.sh
@@ -1,23 +1,71 @@
 #!/bin/bash
 
-for pos in True False
+
+#for lookback in 6 12 24 48 72 96 120 144
+#do
+#  for future in 1 12 24 96 192 336 720
+#  do
+#    if [ $lookback -gt 48 ]
+#    then
+#        python main.py \
+#        --model 'Autoformer' \
+#        --seq_len $lookback \
+#        --pred_len $future
+#    else
+#        python main.py \
+#        --model 'Autoformer' \
+#        --seq_len $lookback \
+#        --label_len 5 \
+#        --pred_len $future
+#    fi
+#  done
+#  rm -r checkpoints
+#done
+
+
+
+
+#for val in True False
+#do
+#  for temp in True False
+#  do
+#    for pos in True False
+#    do
+#      for future in 1 12 48 96 192 336
+#      do
+#        python main.py \
+#        --model 'Autoformer' \
+#        --seq_len 96 \
+#        --pred_len $future \
+#        --positional_embedding $pos \
+#        --value_embedding $val \
+#        --temporal_embedding $temp
+#        rm -r checkpoints
+#      done
+#    done
+#  done
+#done
+
+
+for pos in False
 do
-  for val in True False
+  for val in False
   do
-    for temp in True False
+    for temp in False
     do
-      if [ $pos == False ] && [ $val == False ] && [ $temp == False ]
+      if [ $pos == True ] && [ $val == False ] && [ $temp == False ]
       then
         echo "already done"
         coninue
       else
-        for lookback in 6 12 24 48 72 96 120 144 168 192 336 504 672 720
+        for lookback in 6 72 96 120 144 168 192 336 504 672 720
         do
           for future in 1 12 24 96 192 336 720
           do
             if [ $lookback -gt 48 ]
             then
                 python main.py \
+                --model 'Transformer' \
                 --seq_len $lookback \
                 --pred_len $future \
                 --positional_embedding $pos \
@@ -25,6 +73,7 @@ do
                 --temporal_embedding $temp
             else
                 python main.py \
+                --model 'Transformer' \
                 --seq_len $lookback \
                 --label_len 5 \
                 --pred_len $future \
@@ -39,6 +88,7 @@ do
     done
   done
 done
+
 
 #for lookback in 6 12 24 48 72 96 120 144 168 192 336 504 672 720
 #do

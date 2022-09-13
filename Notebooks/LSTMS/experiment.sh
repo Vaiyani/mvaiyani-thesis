465aacbfff9d48ca1a79c02c38ff3f096114b011
Vaiyani <muneeb.vaiyani@gmail.com>
2022-09-13 16:20:23 +0200

    LSTMS - reduced data set - saved models - results

--- Notebooks/LSTMS/experiment.sh
+++ Notebooks/LSTMS/experiment.sh
@@ -5,6 +5,20 @@ for lookback in 720 672 504 336 192 168 144 120 96 72 48 24 12 6
 do
   for future in 1 12 24 96 192 336 720
   do
-    python main.py --lookback $lookback --future $future
+    python main.py \
+    --lookback $lookback \
+    --future $future \
+    --gpu
+#    if [ $lookback -gt 168 ]
+#    then
+#      python main.py \
+#      --lookback $lookback \
+#      --future $future \
+#      --gpu
+#    else
+#      python main.py \
+#      --lookback $lookback \
+#      --future $future
+#    fi
   done
 done
--- Notebooks/LSTMS/main.py
+++ Notebooks/LSTMS/main.py
@@ -2,37 +2,46 @@ import pandas as pd
 import numpy as np
 import argparse
 import random
+import matplotlib.pyplot as plt
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
-# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '-1'
+
+parser = argparse.ArgumentParser(description='Time Series Forecasting With LSTMS')
+parser.add_argument('--lookback', type=int, required=False, default=6, help='past time step to look into')
+parser.add_argument('--future', type=int, required=False, default=1, help='time steps to predict in future')
+parser.add_argument('--gpu', action='store_true', help='gpu visible')
+args = parser.parse_args()
+lookback = args.lookback
+pred_len = args.future
+gpu = args.gpu
+if gpu:
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    activation = 'tanh'
+else:
+    os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+    activation = 'relu'
+identifier = 'LSTM_Hourly_lookback-{}_future-{}'.format(lookback, pred_len)
+print(identifier)
+print("lookback {} future {} activation {} gpu {}".format(lookback, pred_len, activation, gpu))
+
 import tensorflow as tf
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_absolute_percentage_error, mean_squared_error, mean_absolute_error, r2_score
-from tensorflow.keras.models import save_model, model_from_json, Sequential
+# from tensorflow.keras.models import save_model, model_from_json, Sequential
 from tensorflow.keras.layers import Dense, LSTM, Dropout
 from tensorflow import keras
 from typing import Union
 
-fix_seed = 2022
+fix_seed = 0
 tf.random.set_seed(fix_seed)
 np.random.seed(fix_seed)
 
-# sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True))
-parser = argparse.ArgumentParser(description='Time Series Forecasting With LSTMS')
-parser.add_argument('--lookback', type=int, required=False, default=6, help='past time step to look into')
-parser.add_argument('--future', type=int, required=False, default=1, help='time steps to predict in future')
-args = parser.parse_args()
-lookback = args.lookback
-pred_len = args.future
-identifier = 'LSTM_Hourly_lookback-{}_future-{}'.format(lookback, pred_len)
-print(identifier)
 # isExist = os.path.exists('./saved_models')
 # if not isExist:
 #   os.makedirs('./saved_models')
 
-def min_max_scale(train: np.array, val: np.array, test: np.array) -> Union[MinMaxScaler, np.array, np.array, np.array]:
+def min_max_scale(train: np.array, val: np.array, test: np.array) -> Union[StandardScaler, np.array, np.array, np.array]:
     """ Tranform the train and test data into min max scale of train data"""
-    scaler = MinMaxScaler()
+    scaler = StandardScaler()
     scaler = scaler.fit(train)
     train_normalized = scaler.transform(train)
     test_normalized = scaler.transform(test)
@@ -73,9 +82,9 @@ def sliding_window(data: [], window_length: int, pred_len: int = 1) -> Union[np.
 def model():
 
     model = keras.Sequential()
-    model.add(LSTM(100, activation='tanh', return_sequences=True))
+    model.add(LSTM(100, activation=activation, return_sequences=True))
     model.add(Dropout(0.2))
-    model.add(LSTM(100, activation='tanh'))
+    model.add(LSTM(100, activation=activation))
     model.add(Dense(pred_len))
 
     model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.0001),
@@ -83,13 +92,13 @@ def model():
 
     return model
 
-def show_best_hyperparamters(best_hps):
-    print('Best Hyper Parameters\n')
-    print('Layer 1 neuron: ', best_hps.get('first_layer_neurons'))
-    print('Layer 2 neuron: ', best_hps.get('second_layer_neurons'))
-    print('Activation Function: ', best_hps.get('activationfunc'))
-    print('learning_rate: ', best_hps.get('learning_rate'))
-    print('Dropout rate: ', best_hps.get('dropout'))
+# def show_best_hyperparamters(best_hps):
+#     print('Best Hyper Parameters\n')
+#     print('Layer 1 neuron: ', best_hps.get('first_layer_neurons'))
+#     print('Layer 2 neuron: ', best_hps.get('second_layer_neurons'))
+#     print('Activation Function: ', best_hps.get('activationfunc'))
+#     print('learning_rate: ', best_hps.get('learning_rate'))
+#     print('Dropout rate: ', best_hps.get('dropout'))
 
 
 def calculate_metrics(test: np.ndarray, predict: np.ndarray, identifier: str) -> float:
@@ -100,7 +109,7 @@ def calculate_metrics(test: np.ndarray, predict: np.ndarray, identifier: str) ->
     MAPE = mean_absolute_percentage_error(test.flatten(), predict.flatten())
     r2 = r2_score(test.flatten(), predict.flatten())
     print('mse: {}, mae: {}, rmse: {}, mape: {}, R2: {}'.format(MSE, MAE, RMSE, MAPE, r2))
-    f = open("result.txt", 'a')
+    f = open("result-lstm-reduced-data.txt", 'a')
     f.write(identifier + "  \n")
     f.write('mse: {}, mae: {}, rmse: {}, mape: {}, R2: {}'.format(MSE, MAE, RMSE, MAPE, r2))
     f.write('\n')
@@ -109,7 +118,7 @@ def calculate_metrics(test: np.ndarray, predict: np.ndarray, identifier: str) ->
 
 
 
-df = pd.read_csv('cleaned_data.csv')
+df = pd.read_csv('reduced_data.csv')
 data = df['close'].values
 data = data.reshape(-1,1)
 
@@ -120,7 +129,9 @@ scaler, train_normalized, val_normalized, test_normalized = min_max_scale(train,
 x_train, y_train = sliding_window(train_normalized, lookback, pred_len)
 x_val, y_val = sliding_window(val_normalized, lookback, pred_len)
 x_test, y_test = sliding_window(test_normalized, lookback, pred_len)
-
+print("train :",x_train.shape)
+print("Val :", x_val.shape)
+print("Test :", x_test.shape)
 
 # Build the model with the best hp.
 # model = model_builder(best_hps)
@@ -128,7 +139,6 @@ model = model()
 
 # stop_training_early = keras.callbacks.EarlyStopping()
 stop_training_early = keras.callbacks.EarlyStopping(monitor="val_loss", patience=3)
-# with tf.device('/cpu:0'):
 history = model.fit(x_train, y_train, epochs=60 , verbose=1, shuffle=False, validation_data=(x_val, y_val),
                     callbacks=[stop_training_early])
 
@@ -137,3 +147,8 @@ history = model.fit(x_train, y_train, epochs=60 , verbose=1, shuffle=False, vali
 # model.save('saved_models/' + identifier)
 y_predict = model.predict(x_test)
 calculate_metrics(y_test, y_predict, identifier)
+# assert  1==1
+plt.plot(y_test.flatten(), label='test')
+plt.plot(y_predict.flatten(), 'r-', label='predict')
+plt.legend()
+plt.show()

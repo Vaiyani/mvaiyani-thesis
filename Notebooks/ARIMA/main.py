5211f9c9ba9541e57bb97b2a78969d554eec62da
Vaiyani <muneeb.vaiyani@gmail.com>
2022-10-10 20:28:16 +0200

    ARIMA modelling testing

---
+++ Notebooks/ARIMA/main.py
@@ -0,0 +1,45 @@
+import pmdarima as pm
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.stattools import adfuller
+import statsmodels.api as sm
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from numpy import log
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+
+
+threshold = -15
+df = pd.read_csv('./cleaned_data.csv')
+df_ = np.log(df['close'].values)
+train = df_[:threshold]
+test = df_[threshold:]
+scaler = StandardScaler()
+train = scaler.fit_transform(train.reshape(-1,1)).squeeze()
+test = scaler.transform(test.reshape(-1,1)).squeeze()
+
+model = pm.auto_arima(train,
+                      start_p=10,
+                      start_q=10,
+                      test='adf',       # use adftest to find optimal 'd'
+                      max_p=10,
+                      max_q=10,
+                      m=24,             # frequency of series
+                      d=None,           # let model determine 'd'
+#                       seasonal=False,   # No Seasonality
+                      trace=True,
+                      error_action='ignore',
+                      suppress_warnings=True,
+                      stepwise=False)
+
+print(model.summary())
+fc = model.predict(n_periods=-threshold)
+
+appended_org = np.concatenate((train[-20:],test))
+appended_fc = np.concatenate((train[-20:],fc))
+
+plt.plot(appended_org)
+plt.plot(appended_fc)
+plt.savefig('result.png')
